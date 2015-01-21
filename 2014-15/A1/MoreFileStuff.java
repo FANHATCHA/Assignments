16c3e824d419663f27c491c2b251090a42dd33f8
william-fiset <fisetwill@gmail.com>
2015-01-21 18:19:44 -0400

    -a

--- 2014-15/A1/MoreFileStuff.java
+++ 2014-15/A1/MoreFileStuff.java
@@ -7,17 +7,39 @@
  * 
  **/
 
-
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.io.*;
 
 public class MoreFileStuff {
 
-	public static void modifyBtyes() {
+	/* Opens up a JFileChooser for the user to choose a file from their file system */
+	public static File selectFile () {
+
+		JFileChooser fileChooser = new JFileChooser();
+		int returnVal = fileChooser.showOpenDialog(null);
+
+		if(returnVal == JFileChooser.APPROVE_OPTION) 
+			return fileChooser.getSelectedFile();
+		return null;
 		
 	}
 
-	public static void main(String[] args) {
-		
+	/** 
+	 * This method:
+	 * - Prompts the user for the name of an input file
+	 * - Prompts the user for the index of a byte to be modified in the file they chose
+	 * - Then prompts user for a new byte value to be placed in that location
+	 * @return void 
+	 */
+	public static void modifyBtyes() {
 
+	}
+
+	public static void main(String[] args) {
+
+		modifyBtyes();
 
 	}
 
