661fe64378c6feabf9f2f682a32c8ab1c7396580
william-fiset <fisetwill@gmail.com>
2015-03-13 13:32:19 -0300

    Micah polished up dem Complex class (Will had some bugs)

--- 2014-15/A3/Complex.java
+++ 2014-15/A3/Complex.java
@@ -1,8 +1,74 @@
+/**
 
+@author William Fiset, Micah Stairs
+
+Mandelbrot Set is a region of the Complex plane.
+a + bi --> Complex number
+
+z = z^2 + c
+
+z starts at 0
+c is a constant
+
+**/
 public class Complex {
 
-	public Complex( double inReal, double inImag ) {
+	private double real, imag;
 
+	// Standard constructor
+	public Complex( double inReal, double inImag ) {
+		this.real = inReal;
+		this.imag = inImag
+	}
+
+	// Copying constructor
+	public Complex(Complex inComplex) {
+		this.real = inComplex.getReal();
+		this.imag = inComplex.getImag();
+	}
+
+
+	public void multiply(Complex toMult ) {
+		this.real = real.getReal()*toMult.getReal() - real.getImag()*toMult.getImag();
+		this.imag = real.getImag()*toMult.getReal() + real.getReal()*toMult.getImag();
+	}
+
+	public void add( Complex toAdd ) {
+		this.real += toSub.getReal();
+		this.imag += toSub.getImag();
+	}
+
+	public void subtract(Complex toSub) {
+		this.real -= toSub.getReal();
+		this.imag -= toSub.getImag();
+	}
+
+	public double getImag() {
+		return this.inImag;
+	}
+
+	public double getReal() {
+		return this.inReal;
+	}
+
+	// Returns the modulus of this complex number (distance from origin)
+	public double modulus() {
+		return Math.sqrt(Math.pow(getReal(), 2) + Math.pow(getImag(), 2));
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 2014-15/A3/MandelbrotGUI.java
+++ 2014-15/A3/MandelbrotGUI.java
@@ -1,8 +1,51 @@
+/**
+
+@author William Fiset, Micah Stairs
+
+Each time you render the mandelbrot set, you will iterate though
+all the pixels on the drawing surface (x,y) and convert those
+to positions on the complex plane.
+	
+c = a + bi
+
+*/
+
 
 public class MandelbrotGUI {
 
     public static void main(String[] args) {
 		
+    }
+
+    private int doIteractions( Complex c ) {
+
+    	/*
+
+		if this sequence goes to infinity, then it is NOT in the mandelbrot set
+		otherwise it is in the mandelbrot. 
+
+    	z_0 = 0
+    	z_1 = 0^2 + c = 2
+    	...
+		z_n = (z_n-1)^2 + c
+
+		Simplifications:
+		- If sequence ever leaves a circle of radius two, then 
+		sequence will go to infinity, so stop iterating.
+
+		- If it never leaves, (MAX_ITER ~ 100) then original point
+		c is in the mandelbrot set.
+
+		NOTE ON COLOR: 
+		- Use distance formula to calculate the distance from the
+		origin: dist = (a^2 + b^2) <= 2.
+		- Set ranges for the # of iterations, ex:
+		0-10 purple, 10-20 pink, 20-30 red.... 
+		
+		^^^ 0-10 is the background color
+
+    	*/
+
     }
 
 }
