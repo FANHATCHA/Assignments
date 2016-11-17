d9b10570799692dc6383f9d035a06ca76bc765d9
Micah Stairs <micah.stairs@gmail.com>
2016-11-17 14:21:09 -0400

    setup testing

---
+++ 2016/LW1/TestStore.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class TestStore {
+  
+  /** This is called before each test case. */
+  @Before public void setup() {
+
+  }
+
+  @Test public void testMethod() {
+
+    assertEquals(1, 1);
+    assertTrue(true);
+    assertFalse(false);
+
+  }
+
+}
