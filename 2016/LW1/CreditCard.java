1d05d56b11f1dbef1fe9c0c29485b29164b58182
Micah Stairs <micah.stairs@gmail.com>
2016-11-17 22:16:02 -0400

    added more test cases

--- 2016/LW1/CreditCard.java
+++ 2016/LW1/CreditCard.java
@@ -3,12 +3,14 @@ import java.time.*;
 
 public class CreditCard implements PaymentMethod {
 
+  private final String cardNumber;
   private final Address billingAddress;
   private final String name;
   private final int securityCode;
   private final YearMonth expirationDate;
 
-  public CreditCard(Address billingAddress, String name, int securityCode, YearMonth expirationDate) {
+  public CreditCard(String cardNumber, Address billingAddress, String name, int securityCode, YearMonth expirationDate) {
+    this.cardNumber = cardNumber;
     this.billingAddress = billingAddress;
     this.name = name;
     this.securityCode = securityCode;
@@ -17,6 +19,10 @@ public class CreditCard implements PaymentMethod {
 
   @Override public boolean charge(BigInteger cost) {
     return true;
+  }
+
+  public String getCardNumber() {
+    return cardNumber;
   }
 
   public Address getBillingAddress() {
--- 2016/LW1/TestStore.java
+++ 2016/LW1/TestStore.java
@@ -1,12 +1,30 @@
 import java.math.*;
+import java.time.*;
 import static org.junit.Assert.*;
 import org.junit.*;
 
 public class TestStore {
   
+  // Sample address
+  private static final Address ADDRESS
+    = new Address("62 York Street", "Sackville", "NB", "Canada", "E4L 1E2");
+
+  // Sample items
+  private static final Item ITEM_1 = Item.CLEO_LOLLIPOP;
+  private static final Item ITEM_2 = Item.BOOGIE_WONDERLAND_SLIPPERS;
+
+  // Sample credit card
+  private static final String CARD_NUMBER = "4111 1111 1111 1111";
+  private static final String NAME = "John Smith";
+  private static final int SECURITY_CODE = 901;
+  private static final YearMonth EXPIRATION_DATE = YearMonth.of(18, 7);
+  private static final PaymentMethod PAYMENT_METHOD
+    = new CreditCard(CARD_NUMBER, ADDRESS, NAME, SECURITY_CODE, EXPIRATION_DATE);
+
+  // Reference to the store
   private Store store;
 
-  /** This is called before each test case. */
+  /** This resets the store before each test case. */
   @Before public void setup() {
     store = new Store();
   }
@@ -19,13 +37,13 @@ public class TestStore {
     assertEquals(store.getTotalCost(), expectedTotal);
 
     // Add one item and check total cost
-    store.addItemToCart(Item.NEMO_LOLLIPOP);
-    expectedTotal = expectedTotal.add(Item.NEMO_LOLLIPOP.getCost());
+    store.addItemToCart(ITEM_1);
+    expectedTotal = expectedTotal.add(ITEM_1.getCost());
     assertEquals(store.getTotalCost(), expectedTotal);
 
     // Add another item and check total cost
-    store.addItemToCart(Item.BOOGIE_WONDERLAND_SLIPPERS);
-    expectedTotal = expectedTotal.add(Item.BOOGIE_WONDERLAND_SLIPPERS.getCost());
+    store.addItemToCart(ITEM_2);
+    expectedTotal = expectedTotal.add(ITEM_2.getCost());
     assertEquals(store.getTotalCost(), expectedTotal);
 
   }
@@ -36,25 +54,45 @@ public class TestStore {
     BigInteger expectedTotal = BigInteger.ZERO;
 
     // Add an item to the cart
-    store.addItemToCart(Item.NEMO_LOLLIPOP);
-    expectedTotal = expectedTotal.add(Item.NEMO_LOLLIPOP.getCost());
+    store.addItemToCart(ITEM_1);
+    expectedTotal = expectedTotal.add(ITEM_1.getCost());
     assertEquals(store.getTotalCost(), expectedTotal);
 
     // Add the same item to the cart again
-    store.addItemToCart(Item.NEMO_LOLLIPOP);
-    expectedTotal = expectedTotal.add(Item.NEMO_LOLLIPOP.getCost());
+    store.addItemToCart(ITEM_1);
+    expectedTotal = expectedTotal.add(ITEM_1.getCost());
     assertEquals(store.getTotalCost(), expectedTotal);
 
   }
+
+  /** Tests placing an order which did not specify a payment method. */
+  @Test public void testOrderingWithoutPaymentMethod() {
+
+    store.addItemToCart(ITEM_1);
+    store.enterShippingAddress(ADDRESS);
+    assertNull(store.placeOrder());
+
+  }
+
+  /** Tests placing an order which did not specify a shipping address. */
+  @Test public void testOrderingWithoutShippingAddress() {
+
+    store.addItemToCart(ITEM_1);
+    store.enterPaymentMethod(PAYMENT_METHOD);
+    assertNull(store.placeOrder());
+
+  }
+
+  // test ordering with no payment method
+
+  // test ordering with no shipping address  
 
   // test ordering no items
 
   // test ordering items
   // test ordering repeated items
 
-  // test ordering with no shipping address  
 
-  // test ordering with no payment method
 
   // test ordering with bad payment method (low balance)
 
