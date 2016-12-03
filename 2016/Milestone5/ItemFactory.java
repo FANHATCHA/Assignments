a9aecfcde7029c19d5d4f10a751be7a5ccc6238b
dchaboyer <dcchaboyer@mta.ca>
2016-12-03 15:05:55 -0400

    We got an item factory!

---
+++ 2016/Milestone5/ItemFactory.java
@@ -0,0 +1,48 @@
+
+public class ItemFactory {
+
+	public Item createWolfsbane() {
+		return new Item(101, "Wolfsbane", "An ingredient for potion 1");
+	}
+	
+	public Item createUnicornHair() {
+		return new Item(102, "Unicorn Hair", "An ingredient for potion 1");
+	}
+	
+	public Item createStudentsTears() {
+		return new Item(103, "Students' Tears", "An ingredient for potion 1");
+	}
+	
+	public Item createPotionOne() {
+		return new Item(104, "Potion 1", "[INSERT DESCRIPTION HERE]");
+	}
+	
+	public Item createEelEye() {
+		return new Item(105, "Eel's Eye", "An ingredient for Potion 2");
+	}
+	
+	public Item createNightShade() {
+		return new Item(106, "Nightshade", "An ingredient for Potion 2");
+	}
+	
+	public Item createPotionTwo() {
+		return new Item(107, "Potion 2", "[INSERT DESCRIPTION HERE");
+	}
+	
+	public Item createDragonScale() {
+		return new Item(108, "Dragon Scale", "An ingredient for Potion 3");
+	}
+	
+	public Item createHipsterCoffee() {
+		return new Item(109, "Hipster Coffee, you wouldn't know what's in it", "An ingredient for Potion 3");
+	}
+	
+	public Item createMountiePride() {
+		return new Item(110, "Mountie Pride, at least we're not Acadia", "An ingredient for Potion 3");
+	}
+	
+	public Item createPotion3() {
+		return new Item(111, "Potion 3", "[INSERT DESCRIPTION HERE");
+	}
+	
+}
